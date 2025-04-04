ba37fc166726e77876534404a309f550535088cc
Sergei Borzov <l.sergey.borzov.l@gmail.com>
2025-04-04 21:43:58 +0200

    feat: mtl parsing

--- src/assets/import_obj.cpp
+++ src/assets/import_obj.cpp
@@ -176,6 +176,30 @@ static ObjData::Face& GetNextFace(ObjData& objData)
     return objData.faces[objData.faceCount++];
 }
 
+static ObjData::Material& GetNextMaterial(ObjData& objData)
+{
+    if (objData.materialCount >= objData.materialCapacity)
+    {
+        objData.materials = static_cast<ObjData::Material*>(
+            Hls::Realloc(objData.materials, sizeof(ObjData::Material) *
+                                                objData.materialCapacity * 2));
+        objData.materialCapacity *= 2;
+    }
+    return objData.materials[objData.materialCount++];
+}
+
+static const char*& GetNextTexture(ObjData& objData)
+{
+    if (objData.textureCount >= objData.textureCapacity)
+    {
+        objData.textures = static_cast<const char**>(
+            Hls::Realloc(objData.textures,
+                         sizeof(const char*) * objData.textureCapacity * 2));
+        objData.textureCapacity *= 2;
+    }
+    return objData.textures[objData.textureCount++];
+}
+
 static const char* ParseVertex(const char* ptr, ObjData& objData)
 {
     Math::Vec3& next = GetNextVertex(objData);
@@ -281,6 +305,112 @@ static const char* ParseFace(const char* ptr, ObjData& objData)
     return ptr;
 }
 
+static const char* ParseVec3(const char* ptr, Math::Vec3& v)
+{
+    ptr = SkipWhitespace(ptr);
+    for (u32 i = 0; i < 3; i++)
+    {
+        ptr = ParseF32(ptr, v[i]);
+    }
+    return ptr;
+}
+
+static const char* ParseShapeName(const char* ptr, ObjData& objData)
+{
+    ptr = SkipWhitespace(ptr);
+
+    const char* start = ptr;
+    while (!CharIsNewline(*ptr))
+    {
+        ptr++;
+    }
+
+    while (ptr > start && CharIsWhitespace(*(ptr - 1)))
+    {
+        --ptr;
+    }
+
+    ObjData::Shape& shape = objData.shapes[objData.shapeCount - 1];
+    u32 count = MIN(static_cast<u32>(ptr - start), 127u);
+    strncpy(shape.name, start, count);
+    shape.name[count] = '\0';
+    return ptr;
+}
+
+static const char* ParseMaterialName(const char* ptr, ObjData& objData)
+{
+    ptr = SkipWhitespace(ptr);
+
+    const char* start = ptr;
+    while (!CharIsNewline(*ptr))
+    {
+        ptr++;
+    }
+
+    while (ptr > start && CharIsWhitespace(*(ptr - 1)))
+    {
+        --ptr;
+    }
+
+    ObjData::Material& material = GetNextMaterial(objData);
+    material = {};
+    u32 count = MIN(static_cast<u32>(ptr - start), 127u);
+    strncpy(material.name, start, count);
+    material.name[count] = '\0';
+    return ptr;
+}
+
+static const char* ParseTexture(const char* ptr, ObjData& objData,
+                                i32& textureHandle)
+{
+    ptr = SkipWhitespace(ptr);
+
+    // TODO: options
+    if (*ptr == '-')
+        return ptr;
+
+    const char* start = ptr;
+    while (!CharIsNewline(*ptr))
+    {
+        ptr++;
+    }
+
+    while (ptr > start && CharIsWhitespace(*(ptr - 1)))
+    {
+        --ptr;
+    }
+
+    u32 count = static_cast<u32>(ptr - start);
+    char* str = static_cast<char*>(Hls::Alloc(sizeof(char) * count + 1));
+    strncpy(str, start, count);
+    str[count] = '\0';
+
+    i32 index = -1;
+    for (u32 i = 0; i < objData.textureCount; i++)
+    {
+        if (strcmp(objData.textures[i], str) == 0)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1)
+    {
+        index = objData.textureCount;
+        const char*& texture = GetNextTexture(objData);
+        texture = str;
+    }
+    else
+    {
+        Hls::Free(str);
+    }
+
+    textureHandle = index;
+
+    return ptr;
+}
+
 static const char* ParseMtlLib(const char* ptr, ObjData& objData)
 {
     Arena& scratch = GetScratchArena();
@@ -306,12 +436,206 @@ static const char* ParseMtlLib(const char* ptr, ObjData& objData)
     const char* p = str.Data();
     const char* mtlEnd = str.Data() + str.Size();
 
+    bool isAlphaPresent = false;
     while (p != mtlEnd)
     {
-        ArenaMarker loopMarker = ArenaGetMarker(scratch);
-        const char* np = SkipLine(p);
-        p = np;
-        ArenaPopToMarker(scratch, loopMarker);
+        p = SkipWhitespace(p);
+        switch (*p)
+        {
+            case 'n':
+            {
+                p++;
+                if (p[0] == 'e' && p[1] == 'w' && p[2] == 'm' && p[3] == 't' &&
+                    p[4] == 'l' && Hls::CharIsWhitespace(p[5]))
+                {
+                    p = ParseMaterialName(p + 5, objData);
+                    isAlphaPresent = false;
+                }
+                break;
+            }
+            case 'K':
+            {
+                if (p[1] == 'a')
+                {
+                    p = ParseVec3(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].ambient);
+                }
+                else if (p[1] == 'd')
+                {
+                    p = ParseVec3(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].diffuse);
+                }
+                else if (p[1] == 's')
+                {
+                    p = ParseVec3(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].specular);
+                }
+                else if (p[1] == 'e')
+                {
+                    p = ParseVec3(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].emission);
+                }
+                else if (p[1] == 't')
+                {
+                    p = ParseVec3(p + 2,
+                                  objData.materials[objData.materialCount - 1]
+                                      .transmittance);
+                }
+                break;
+            }
+            case 'd':
+            {
+                if (CharIsWhitespace(p[1]))
+                {
+                    p = ParseF32(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].alpha);
+                    isAlphaPresent = true;
+                }
+                break;
+            }
+            case 'T':
+            {
+                if (p[1] == 'r')
+                {
+                    if (!isAlphaPresent)
+                    {
+                        f32 tr = 0.0f;
+                        p = ParseF32(p + 2, tr);
+                        objData.materials[objData.materialCount - 1].alpha =
+                            1.0f - tr;
+                    }
+                }
+                else if (p[1] == 'f')
+                {
+                    p = ParseVec3(p + 2,
+                                  objData.materials[objData.materialCount - 1]
+                                      .transmissionFilter);
+                }
+                break;
+            }
+            case 'N':
+            {
+                if (p[1] == 's')
+                {
+                    p = ParseF32(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].shininess);
+                }
+                else if (p[1] == 'i')
+                {
+                    p = ParseF32(
+                        p + 2,
+                        objData.materials[objData.materialCount - 1].ior);
+                }
+                break;
+            }
+            case 'i':
+            {
+                p++;
+                if (p[0] == 'l' && p[1] == 'l' && p[2] == 'u' && p[3] == 'm' &&
+                    CharIsWhitespace(p[4]))
+                {
+                    p = ParseI32(p + 4,
+                                 objData.materials[objData.materialCount - 1]
+                                     .illuminationModel);
+                }
+                break;
+            }
+            case 'm':
+            {
+                p++;
+                if (p[0] == 'a' && p[1] == 'p' && p[2] == '_')
+                {
+                    p += 3;
+                    if (*p == 'K')
+                    {
+                        p++;
+                        if (CharIsWhitespace(p[1]))
+                        {
+                            if (*p == 'a')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .ambientTextureIndex);
+                            }
+                            else if (*p == 'd')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .diffuseTextureIndex);
+                            }
+                            else if (*p == 's')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .specularTextureIndex);
+                            }
+                            else if (*p == 'e')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .emissionTextureIndex);
+                            }
+                            else if (*p == 't')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .transmittanceTextureIndex);
+                            }
+                        }
+                    }
+                    else if (*p == 'N')
+                    {
+                        p++;
+                        if (CharIsWhitespace(p[1]))
+                        {
+                            if (*p == 's')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .shininessTextureIndex);
+                            }
+                            else if (*p == 'i')
+                            {
+                                p = ParseTexture(
+                                    p + 1, objData,
+                                    objData.materials[objData.materialCount - 1]
+                                        .iorTextureIndex);
+                            }
+                        }
+                    }
+                    else if (*p == 'd')
+                    {
+                        p = ParseTexture(
+                            p + 1, objData,
+                            objData.materials[objData.materialCount - 1]
+                                .alphaTextureIndex);
+                    }
+                    else if ((p[0] == 'b' || p[0] == 'B') && p[1] == 'u' &&
+                             p[2] == 'm' && p[3] == 'p' &&
+                             CharIsWhitespace(p[4]))
+                    {
+                        p = ParseTexture(
+                            p + 4, objData,
+                            objData.materials[objData.materialCount - 1]
+                                .bumpTextureIndex);
+                    }
+                }
+                break;
+            }
+        }
+        p = SkipLine(p);
     }
 
     ArenaPopToMarker(scratch, marker);
@@ -319,54 +643,42 @@ static const char* ParseMtlLib(const char* ptr, ObjData& objData)
     return ptr;
 }
 
-static const char* ParseName(const char* ptr, ObjData& objData)
-{
-    ptr = SkipWhitespace(ptr);
-
-    const char* start = ptr;
-    while (!CharIsNewline(*ptr))
-    {
-        ptr++;
-    }
-
-    while (ptr > start && CharIsWhitespace(*(ptr - 1)))
-    {
-        --ptr;
-    }
-
-    ObjData::Shape& shape = objData.shapes[objData.shapeCount - 1];
-    u32 count = MIN(static_cast<u32>(ptr - start), 127u);
-    strncpy(shape.name, start, count);
-    shape.name[count] = '\0';
-    return ptr;
-}
-
 bool ParseObj(String8 str, ObjData& objData)
 {
     objData.vertexCount = 0;
-    objData.vertexCapacity = 200000;
+    objData.vertexCapacity = 131136;
     objData.vertices = static_cast<Math::Vec3*>(
         Hls::Alloc(sizeof(Math::Vec3) * objData.vertexCapacity));
 
     objData.normalCount = 0;
-    objData.normalCapacity = 200000;
+    objData.normalCapacity = 131136;
     objData.normals = static_cast<Math::Vec3*>(
         Hls::Alloc(sizeof(Math::Vec3) * objData.normalCapacity));
 
     objData.texCoordCount = 0;
-    objData.texCoordCapacity = 200000;
+    objData.texCoordCapacity = 131136;
     objData.texCoords = static_cast<Math::Vec2*>(
         Hls::Alloc(sizeof(Math::Vec2) * objData.texCoordCapacity));
 
     objData.faceCount = 0;
-    objData.faceCapacity = 500000;
+    objData.faceCapacity = 131136;
     objData.faces = static_cast<ObjData::Face*>(
         Hls::Alloc(sizeof(ObjData::Face) * objData.faceCapacity));
 
     objData.shapeCount = 0;
-    objData.shapeCapacity = 1000;
+    objData.shapeCapacity = 256;
     objData.shapes = static_cast<ObjData::Shape*>(
         Hls::Alloc(sizeof(ObjData::Shape) * objData.shapeCapacity));
+
+    objData.materialCount = 0;
+    objData.materialCapacity = 64;
+    objData.materials = static_cast<ObjData::Material*>(
+        Hls::Alloc(sizeof(ObjData::Material) * objData.materialCapacity));
+
+    objData.textureCount = 0;
+    objData.textureCapacity = 64;
+    objData.textures = static_cast<const char**>(
+        Hls::Alloc(sizeof(const char*) * objData.textureCapacity));
 
     const char* p = str.Data();
     const char* end = str.Data() + str.Size();
@@ -454,7 +766,7 @@ bool ParseObj(String8 str, ObjData& objData)
                     case ' ':
                     case '\t':
                     {
-                        p = ParseName(p, objData);
+                        p = ParseShapeName(p, objData);
                         break;
                     }
                     case '\n':
@@ -529,6 +841,11 @@ bool ImportWavefrontObj(String8 filepath, ObjData& objData)
 
 void FreeWavefrontObj(ObjData& objData)
 {
+    for (u32 i = 0; i < objData.textureCount; i++)
+    {
+        Hls::Free((void*)objData.textures[i]);
+    }
+    Hls::Free(objData.textures);
     Hls::Free(objData.vertices);
     Hls::Free(objData.normals);
     Hls::Free(objData.texCoords);
--- src/assets/import_obj.h
+++ src/assets/import_obj.h
@@ -32,28 +32,29 @@ struct ObjData
 {
     struct Material
     {
+        char name[128] = {0};
         Math::Vec3 ambient;
         Math::Vec3 diffuse;
         Math::Vec3 specular;
         Math::Vec3 emission;
         Math::Vec3 transmittance;
         Math::Vec3 transmissionFilter;
-        f32 shininess;
-        f32 ior;
-        f32 alpha;
-        i32 illuminationModel;
-        //i32 fallback;
-        u32 ambientTextureHandle;
-        u32 diffuseTextureHandle;
-        u32 specularTextureHandle;
-        u32 emissionTextureHandle;
-        u32 transmittanceTextureHandle;
-        u32 shininessTextureHandle;
-        u32 iorTextureHandle;
-        u32 alphaTextureHandle;
-        u32 bumpTextureHandle;
+        f32 shininess = 0.0f;
+        f32 ior = 0.0f;
+        f32 alpha = 1.0f;
+        i32 illuminationModel = 2;
+
+        i32 ambientTextureIndex = -1;
+        i32 diffuseTextureIndex = -1;
+        i32 specularTextureIndex = -1;
+        i32 emissionTextureIndex = -1;
+        i32 transmittanceTextureIndex = -1;
+        i32 shininessTextureIndex = -1;
+        i32 iorTextureIndex = -1;
+        i32 alphaTextureIndex = -1;
+        i32 bumpTextureIndex = -1;
     };
-    
+
     struct Index
     {
         u32 vertexIndex = 0;
@@ -74,12 +75,13 @@ struct ObjData
     };
 
     String8 objDirectoryPath;
-    String8* textures = nullptr;
+    const char** textures = nullptr;
     Face* faces = nullptr;
     Math::Vec3* vertices = nullptr;
     Math::Vec3* normals = nullptr;
     Math::Vec2* texCoords = nullptr;
     Shape* shapes = nullptr;
+    Material* materials = nullptr;
 
     u32 textureCount = 0;
     u32 vertexCount = 0;
@@ -87,18 +89,14 @@ struct ObjData
     u32 texCoordCount = 0;
     u32 faceCount = 0;
     u32 shapeCount = 0;
+    u32 materialCount = 0;
     u32 textureCapacity = 0;
     u32 vertexCapacity = 0;
     u32 normalCapacity = 0;
     u32 texCoordCapacity = 0;
     u32 faceCapacity = 0;
     u32 shapeCapacity = 0;
-    u32 textureCommitSize = 0;
-    u32 vertexCommitSize = 0;
-    u32 normalCommitSize = 0;
-    u32 texCoordCommitSize = 0;
-    u32 faceCommitSize = 0;
-    u32 shapeCommitSize = 0;
+    u32 materialCapacity = 0;
 };
 bool ImportWavefrontObj(String8 filename, ObjData& objData);
 void FreeWavefrontObj(ObjData& objData);
