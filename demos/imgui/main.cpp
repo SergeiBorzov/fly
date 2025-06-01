9359db3f1f026a012d9821db86db3730ae898691
Sergei Borzov <l.sergey.borzov.l@gmail.com>
2025-06-01 17:50:01 +0200

    feat: imgui demo

---
+++ demos/imgui/main.cpp
@@ -0,0 +1,195 @@
+#include "core/assert.h"
+#include "core/filesystem.h"
+#include "core/log.h"
+#include "core/platform.h"
+#include "core/thread_context.h"
+
+#include "rhi/allocation_callbacks.h"
+#include "rhi/context.h"
+#include "rhi/pipeline.h"
+#include "rhi/shader_program.h"
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_vulkan.h>
+
+#include <GLFW/glfw3.h>
+
+using namespace Fly;
+
+static void OnKeyboardPressed(GLFWwindow* window, int key, int scancode,
+                              int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+static void ErrorCallbackGLFW(i32 error, const char* description)
+{
+    FLY_ERROR("GLFW - error: %s", description);
+}
+
+static void ExecuteCommands(RHI::Device& device)
+{
+    RHI::CommandBuffer& cmd = RenderFrameCommandBuffer(device);
+
+    const RHI::SwapchainTexture& swapchainTexture =
+        RenderFrameSwapchainTexture(device);
+
+    VkRect2D renderArea = {{0, 0},
+                           {swapchainTexture.width, swapchainTexture.height}};
+    VkRenderingAttachmentInfo colorAttachment = RHI::ColorAttachmentInfo(
+        swapchainTexture.imageView, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+    VkRenderingInfo renderInfo =
+        RHI::RenderingInfo(renderArea, &colorAttachment, 1);
+
+    vkCmdBeginRendering(cmd.handle, &renderInfo);
+    ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), cmd.handle);
+    vkCmdEndRendering(cmd.handle);
+}
+
+int main(int argc, char* argv[])
+{
+    InitThreadContext();
+    if (!InitLogger())
+    {
+        return -1;
+    }
+
+    if (volkInitialize() != VK_SUCCESS)
+    {
+        FLY_ERROR("Failed to load volk");
+        return -1;
+    }
+    glfwInitVulkanLoader(vkGetInstanceProcAddr);
+
+    if (!glfwInit())
+    {
+        FLY_ERROR("Failed to init glfw");
+        return -1;
+    }
+    glfwSetErrorCallback(ErrorCallbackGLFW);
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Imgui demo", nullptr, nullptr);
+    if (!window)
+    {
+        FLY_ERROR("Failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetKeyCallback(window, OnKeyboardPressed);
+
+    // Device extensions
+    const char* requiredDeviceExtensions[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+
+    RHI::ContextSettings settings{};
+    settings.instanceExtensions =
+        glfwGetRequiredInstanceExtensions(&settings.instanceExtensionCount);
+    settings.deviceExtensions = requiredDeviceExtensions;
+    settings.deviceExtensionCount = STACK_ARRAY_COUNT(requiredDeviceExtensions);
+    settings.windowPtr = window;
+
+    RHI::Context context;
+    if (!RHI::CreateContext(settings, context))
+    {
+        FLY_ERROR("Failed to create context");
+        return -1;
+    }
+    RHI::Device& device = context.devices[0];
+
+    VkDescriptorPoolSize poolSizes[] = {
+        {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+         IMGUI_IMPL_VULKAN_MINIMUM_IMAGE_SAMPLER_POOL_SIZE},
+    };
+
+    VkDescriptorPool descriptorPool;
+    VkDescriptorPoolCreateInfo poolInfo = {};
+    poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    poolInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+    poolInfo.maxSets = 0;
+    for (VkDescriptorPoolSize& poolSize : poolSizes)
+        poolInfo.maxSets += poolSize.descriptorCount;
+    poolInfo.poolSizeCount = static_cast<u32>(STACK_ARRAY_COUNT(poolSizes));
+    poolInfo.pPoolSizes = poolSizes;
+    if (vkCreateDescriptorPool(device.logicalDevice, &poolInfo,
+                               RHI::GetVulkanAllocationCallbacks(),
+                               &descriptorPool) != VK_SUCCESS)
+    {
+        FLY_ERROR("Failed to create imgui pool");
+        return -1;
+    }
+
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForVulkan(window, true);
+
+    ImGui_ImplVulkan_InitInfo initInfo = {};
+    initInfo.ApiVersion = VK_API_VERSION_1_3;
+    initInfo.Instance = context.instance;
+    initInfo.PhysicalDevice = device.physicalDevice;
+    initInfo.Device = device.logicalDevice;
+    initInfo.Queue = device.graphicsComputeQueue;
+    initInfo.DescriptorPool = descriptorPool;
+    initInfo.MinImageCount = device.swapchainTextureCount;
+    initInfo.ImageCount = device.swapchainTextureCount;
+    initInfo.UseDynamicRendering = true;
+
+    initInfo.PipelineRenderingCreateInfo = {};
+    initInfo.PipelineRenderingCreateInfo.sType =
+        VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
+    initInfo.PipelineRenderingCreateInfo.colorAttachmentCount = 1;
+    initInfo.PipelineRenderingCreateInfo.pColorAttachmentFormats =
+        &device.surfaceFormat.format;
+
+    initInfo.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+    initInfo.Allocator = RHI::GetVulkanAllocationCallbacks();
+
+    FLY_LOG("Init imgui vulkan");
+    if (!ImGui_ImplVulkan_Init(&initInfo))
+    {
+        FLY_ERROR("Failed to init vulkan!");
+        return -1;
+    }
+    FLY_LOG("Create fonts texture vulkan");
+    if (!ImGui_ImplVulkan_CreateFontsTexture())
+    {
+        FLY_ERROR("Failed to create imgui font texture");
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        ImGui_ImplVulkan_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow();
+        ImGui::Render();
+
+        RHI::BeginRenderFrame(device);
+        ExecuteCommands(device);
+        RHI::EndRenderFrame(device);
+    }
+
+    RHI::WaitAllDevicesIdle(context);
+    ImGui_ImplVulkan_Shutdown();
+    vkDestroyDescriptorPool(device.logicalDevice, descriptorPool,
+                            RHI::GetVulkanAllocationCallbacks());
+    RHI::DestroyContext(context);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    FLY_LOG("Shutdown successful");
+    ShutdownLogger();
+
+    ReleaseThreadContext();
+    return 0;
+}
